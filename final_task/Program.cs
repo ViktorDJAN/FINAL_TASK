816d521697cd753f8ab09464ec7e7c696b454382
Viktor St.one <pistoletviktor@gmail.com>
2022-12-27 13:10:30 +0300

    have created the processing block

--- final_task/Program.cs
+++ final_task/Program.cs
@@ -14,3 +14,26 @@ string[] Getarray(int[] num)
     Console.WriteLine($"[{string.Join(",", arr1)}]");
     return arr1;
 }
+int[] arr1 = new int[0];
+string[] a = Getarray(arr1);
+
+
+int numsel = new Random().Next(0, 4);
+
+
+
+string[] mainwork(string[] curr, string[] changed)
+{
+    string[] exp = new string[numsel];
+    int rand = new Random().Next(0, curr.Length);
+
+    for (int i = 0; i < numsel; i++)
+    {
+        string t = curr[rand];
+
+        curr[rand] = curr[i];
+        exp[i] = t;
+    }
+
+    return exp;
+}
